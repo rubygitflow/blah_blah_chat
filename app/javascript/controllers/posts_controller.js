eed77b1496b3c3dcfd9039f92f13ce5d101f3068
rubygitflow <nrubygitflow@gmail.com>
2023-10-14 20:07:52 +0600

    add sound

--- app/javascript/controllers/posts_controller.js
+++ app/javascript/controllers/posts_controller.js
@@ -1,14 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['post', 'link']
+  static targets = ['post']
   initialize() {
     this.chat = document.getElementById('showChat')
+    this.audio = new Audio(window.location.origin + '/513269__zhr__tl-light-on-e.mp3');
   }
 
   postTargetConnected(e) {
     let p = this.postTargets[0].innerText.split('>')[1]
     let c = this.chat.innerText
     this.dispatch('copy', { detail: { chat: c, post: p } })
+    this.audio.play();
   }
 }

b48f17d88b9536a248b3aac2d7ac4473c9f2bd9d
rubygitflow <nrubygitflow@gmail.com>
2023-10-17 08:58:15 +0600

    add the use of Stimulus values

--- app/javascript/controllers/toast_controller.js
+++ app/javascript/controllers/toast_controller.js
@@ -3,24 +3,21 @@ import { Toast } from 'bootstrap'
 
 export default class extends Controller {
   static targets = ['toast']
+  static values = {
+    id: String
+  }
 
   initialize() {
     this.audio = new Audio(window.location.origin + '/513269__zhr__tl-light-on-e.mp3')
     this.toast = new Toast(
       document.getElementById('receiveToast')
     )
-    this.toast_bak = toast.innerText.trim()
-    this.toast_text = this.toast_bak
   }
 
-  toastTargetConnected(e) {
-    this.toast_bak  = e.innerText.trim()
-    if (this.toast_bak === this.toast_text) {} else {
-      this.toast_text = this.toast_bak
-      if (this.toast_text === '') {} else {
-        this.toast.show()
-        this.audio.play()
-      }
+  idValueChanged(value, previousValue) {
+    if (!(value === '')) {
+      this.toast.show()
+      this.audio.play()
     }
   }
 }
